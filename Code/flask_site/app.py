156b82debd9f5979adddd4ad1bccaea5806d931b
sosullivan7221 <sosullivan7221@gmail.com>
2023-10-11 23:47:22 +0000

    created flask site + screenshots

--- Code/flask_site/app.py
+++ Code/flask_site/app.py
@@ -1,8 +1,10 @@
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, inspect, text
 import sqlalchemy
-import pandas as pd
 import os
 from dotenv import load_dotenv 
+from flask import Flask, render_template, request
+
+load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
 DB_DATABASE = os.getenv("DB_DATABASE")
@@ -14,11 +16,29 @@ conn_string = (
     f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}")
 engine = create_engine(conn_string)
 
-def get_tables(engine):
-    """Get list of tables."""
-    inspector = inspect(engine)
-    return inspector.get_table_names()
+app = Flask(__name__)
 
-output = get_tables(engine)
-print(output)
-    
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/patients')
+def patients():
+    with engine.connect() as connection:
+        query_1 = text('SELECT * FROM patients')
+        result_1 = connection.execute(query_1)
+        table_1 = result_1.fetchall()
+
+    return render_template('patients.html', data1=table_1)
+
+@app.route('/medication')
+def medication():
+    with engine.connect() as connection:
+        query_2 = text('SELECT * FROM patient_medication')
+        result_2 = connection.execute(query_2)
+        table_2 = result_2.fetchall()
+
+    return render_template('medication.html', data2=table_2)
+
+if __name__ == '__main__':
+    app.run(debug=True)
